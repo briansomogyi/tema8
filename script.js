1d40807c1378dc3a374dab1f9783d19b8a044b8c
briansomogyi <brian.somogyi@emanuel.ro>
2024-04-08 15:10:17 +0300

    Am facut matricea.

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(560, 560);
+    noLoop();
 }
 
 function draw() {
-    background(220);
+    let count = 0;
+    for (let y = 0; y < 7; y++) {
+        for (let x = 0; x < 4; x++) {
+            let posX = x * 140;
+            let posY = y * 80;
+            fill(random(255), random(255), random(255));
+            rect(posX, posY, 140, 80);
+            fill(0);
+            textSize(32);
+            textAlign(CENTER, CENTER);
+            text(++count, posX + 70, posY + 40);
+        }
+    }
 }
